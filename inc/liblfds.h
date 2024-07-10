1dce88f4ea4c22e4da460a97af1f9af7e5e9ac69
luogang <gang.luo@bst.ai>
2024-07-10 10:46:20 +0800

    fix: fix some file name error.

--- inc/liblfds.h
+++ inc/liblfds.h
@@ -19,7 +19,7 @@
   #include "liblfds/lfds_freelist.h"
   #include "liblfds/lfds_hash_addonly.h"
   #include "liblfds/lfds_list_addonly_single_order.h"
-  #include "liblfds/lfds_list_addonly_single_order.h"
+  #include "liblfds/lfds_list_addonly_single_unorder.h"
   #include "liblfds/lfds_queue_bounded_multi2multi.h"
   #include "liblfds/lfds_queue_bounded_single2single.h"
   #include "liblfds/lfds_queue_unbounded_multi2multi.h"
--- inc/liblfds/lfds_list_addonly_single_unorder.h
+++ inc/liblfds/lfds_list_addonly_single_unorder.h
@@ -1,5 +1,5 @@
-#ifndef _LFDS_LIST_ADDONLY_SINGLY_UNORDER_H_
-#define _LFDS_LIST_ADDONLY_SINGLY_UNORDER_H_
+#ifndef _LFDS_LIST_ADDONLY_SINGLE_UNORDER_H_
+#define _LFDS_LIST_ADDONLY_SINGLE_UNORDER_H_
 
 /***** defines *****/
 #define LFDS_LIST_ASU_GET_START( list_asu_state )                                             ( LFDS_MISC_BARRIER_LOAD, (list_asu_state).start->next )
@@ -91,4 +91,4 @@ void lfds_list_asu_query( struct lfds_list_asu_state *lasus,
                              void *query_input,
                              void *query_output );
 
-#endif // _LFDS_LIST_ADDONLY_SINGLY_UNORDER_H_
+#endif // _LFDS_LIST_ADDONLY_SINGLE_UNORDER_H_
